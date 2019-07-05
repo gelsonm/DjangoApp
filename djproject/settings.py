fa28c5fc82b70c2f8147694d94f7acbf5ee36f61
gelsonm <gelsonvamp@gmail.com>
2019-07-05 16:35:25 +0530

    database changed

--- djproject/settings.py
+++ djproject/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'djproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
+'''
 DATABASES = {
     'default':{
         'ENGINE':'django.db.backends.mysql',
@@ -92,7 +92,7 @@ DATABASES = {
         'PORT':'3306'
     }
 }
-
+'''
 
 
 
